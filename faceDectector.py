60b80c8be4ff123f7e576045bf5d15ecc1ce5e50
g003361711996 <56116283+g003361711996@users.noreply.github.com>
2019-12-06 22:29:38 +0000

    Create faceDectector.py

---
+++ faceDectector.py
@@ -0,0 +1,33 @@
+import cv2
+import face_recognition
+
+#webcam 
+video_capture = cv2.VideoCapture("/dev/video1")
+
+face_locations = []
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find faces in video
+    face_locations = face_recognition.face_locations(rgb_frame)
+
+    # Display results
+    for top, right, bottom, left in face_locations:
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+    # Display image
+    cv2.imshow('Video', frame)
+
+    # quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
