e81f027dd9a405c87068abdb04fcd3596787d3a2
g003361711996 <g00336171@gmit.ie>
2019-12-22 13:45:47 +0000

    Pi Transcribe ammend

--- audio_transcriber.py
+++ audio_transcriber.py
@@ -2,7 +2,7 @@ import speech_recognition as sr     # import the library
 
 r = sr.Recognizer()                 # initialize recognizer
 with sr.Microphone() as source:     # mention source it will be either Microphone or audio files.
-    print("Speak Anything :")
+    print("Lecture is being recorded and Transcribed :")
     audio = r.listen(source)        # listen to the source
     try:
         text = r.recognize_google(audio)    # use recognizer to convert our audio into text part.
