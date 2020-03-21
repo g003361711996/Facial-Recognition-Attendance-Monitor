1ad15d8969438c73ae857280f05cfe0b212aeac8
g003361711996 <g00336171@gmit.ie>
2020-03-21 12:12:32 +0000

    addPeople ammend

--- trainFace/addPeople.py
+++ trainFace/addPeople.py
@@ -23,3 +23,21 @@ def addPeople():
     conn.close()
     return nameAndID
 
+# adds faces to the created people in PersonGroup
+def addFaceToPerson(list):
+    headers = {'Content-Type': 'application/octet-stream', 'Ocp-Apim-Subscription-Key':KEY}
+    conn = http.client.HTTPSConnection('projectface.cognitiveservices.azure.com')
+    for item in list:
+        params = urllib.parse.urlencode({'personGroupId': group_id, 'personId': item[1]}) # item[1] is the personId created from addPeople()
+        directory = '/home/pi/Desktop/ProjectSamples/People/'+item[0] # item[0] is peron's name, each person should have a directory named after them filled with photos of them
+        for filename in os.listdir(directory):
+            if filename.endswith('.jpeg'): # adjust this depending on the file type of your photos
+                filePath = os.path.join(directory, filename) # creates full file path
+                body = open(filePath,'rb')
+                conn.request("POST", "/face/v1.0/persongroups/{employees}/persons/"+item[1]+"/persistedFaces?%s" % params, body, headers)
+                response = conn.getresponse()
+                data = json.loads(response.read()) # successful run will print persistedFaceId
+                print(data)
+    conn.close()
+
+addFaceToPerson(addPeople())
