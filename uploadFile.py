0231e8b93d6ac3a1c08bb32984dbafaf4aa101ba
g003361711996 <56116283+g003361711996@users.noreply.github.com>
2020-04-15 01:13:39 +0100

    Add files via upload
    Upload To s3 AWS

---
+++ uploadFile.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+
+bucketName = 'mybuckfucket'
+
+s3=boto3.resource('s3')
+directory = '/home/pi/Desktop/'
+fileName = 'pic.jpeg'
+filePath = os.path.join(directory,fileName)
+
+data = open(filePath, 'rb')
+s3.Bucket(bucketName).put_object(Key=fileName, Body=data, ContentType='image/jpeg')
+object_acl = s3.ObjectAcl(bucketName, fileName)
+response = object_acl.put(ACL='public-read') # makes link public
+
+link = 'https://s3.amazonaws.com/'+bucketName+'/'+fileName # links in s3 follow this pattern
+print(link)
